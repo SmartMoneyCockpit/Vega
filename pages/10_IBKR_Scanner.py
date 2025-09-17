0a25dc328eaaa452105433487585ba29adfd15c0
SmartMoneyCockpit <blainedares@gmail.com>
2025-09-17 08:55:15 -0700

    Delete pages directory

--- pages/10_IBKR_Scanner.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from modules.ui.ibkr_scanner_panel import render
-st.set_page_config(page_title='IBKR Scanner', layout='wide')
-st.title('IBKR Stock Scanner (Environment-Aware)')
-render()
