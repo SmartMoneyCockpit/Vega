f93e30a896fe03d43000a6e2b6b2d523aff0598f
SmartMoneyCockpit <blainedares@gmail.com>
2025-10-01 10:10:58 -0500

    Delete xupload directory

--- xupload/remove_tws_autorestart.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM --- This script removes the scheduled task that auto-restarts TWS ---
-
-schtasks /Delete /TN "Restart TWS" /F
-echo Scheduled task 'Restart TWS' has been removed.
-pause
